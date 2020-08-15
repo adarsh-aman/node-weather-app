57e218c3c18daaeb36cc9b31cf6eb6fffc7dd871
adarsh-aman <aman.rishu98@gmail.com>
2020-08-16 01:37:53 +0530

    setup for heroku

--- src/app.js
+++ src/app.js
@@ -6,7 +6,7 @@ const forecast = require('./utils/forecast')
 // console.log(__dirname)
 // console.log(path.join(__dirname,'../public'))
 const app = express();
-
+const port = process.env.PORT || 3000;
 //define Paths for Express config
 const publicDirectoryPath = path.join(__dirname,'../public')
 const viewsPath = path.join(__dirname, '../templates/views');
@@ -90,6 +90,6 @@ app.get('*',(req,res)=>{
         errorMessage: '404 Page not found !'
     })
 })
-app.listen(3000, ()=>{
-    console.log("Server is up on port 3000 !")
+app.listen(port, ()=>{
+    console.log("Server is up on port " + port)
 })
